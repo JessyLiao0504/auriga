39048a4a0a7a4a6f814e3d1aa8b3e2d201963aa0
linsly-achang <76680239+linsly-achang@users.noreply.github.com>
2022-09-02 18:04:54 +0800

    dev

--- js/main.js
+++ js/main.js
@@ -1,3 +1,8 @@
+const aurigaConfigs = {
+    obsSelector: '.obs>li',
+    prevPos: 0,
+};
+
 function scrollToTarget(selector = '') {
     if (selector) {
         if (document.querySelector(selector)) document.querySelector(selector).scrollIntoView({
@@ -6,15 +11,53 @@ function scrollToTarget(selector = '') {
     }
 }
 
+function registerObserver(selector) {
+    window.addEventListener('scroll', eve => {
+        const activeItem = document.querySelector(`${selector}.active`);
+
+        let items = Array.from(document.querySelectorAll(`${selector}`));
+        let currentItem;
+        let pos, lastPos;
+        
+        for (let e of items) {
+            pos = e.getBoundingClientRect().top - window.innerHeight / 2;
+            if (pos < 0) {
+                if (!currentItem || Math.abs(pos) < Math.abs(lastPos)) {
+                    currentItem = e;
+                    lastPos = pos;
+                }
+            } else {
+                if (e.classList.contains('active')) {
+                    e.classList.remove('active');
+                }
+            }
+        }
+
+        if(currentItem){
+            if (currentItem !== activeItem) {
+                document.querySelectorAll(`${selector}.active`).forEach(el => el.classList.remove('active'));
+                currentItem.classList.add('active');
+            }
+        }
+    });
+}
+
 window.addEventListener('mousemove', e => {
     const elem = document.createElement('div');
     const spread = Math.round(Math.random() * 8) + 3;
     const blur = spread * (Math.round(Math.random() * 12) + 2);
     elem.classList.add('absolute', 'bg-light-green', 'inline-block', 'rounded-full', 'play-dot');
-    elem.style = `top: ${e.pageY}; left: ${e.pageX}; box-shadow: 0 0 ${blur}px ${spread}px rgba(57, 240, 185, .9); filter: hue-rotate(${Math.random()*180}deg) brightness(${Math.random()*1+0.3});`;
+    elem.style = `top: ${e.pageY}; left: ${e.pageX}; box-shadow: 0 0 ${blur}px ${spread}px rgba(57, 240, 185, .9); filter: hue-rotate(${Math.random() * 180}deg) brightness(${Math.random() * 1 + 0.3});`;
 
     document.getElementById('bg-play').append(elem);
     setTimeout(function () {
         elem.remove();
     }, 1000);
 });
+
+// Set things up
+window.addEventListener("load", () => {
+    document.querySelectorAll(aurigaConfigs.obsSelector).forEach(e => {
+        //createObserver(e);
+    });
+}, false);
