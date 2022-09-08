1cde36d55de14282a4774710216260eb322caa28
linsly-achang <76680239+linsly-achang@users.noreply.github.com>
2022-09-08 11:52:47 +0800

    dev

--- js/main.js
+++ js/main.js
@@ -1,6 +1,8 @@
 const aurigaConfigs = {
     obsSelector: '.obs>li',
     prevPos: 0,
+    pointerX: 0,
+    pointerY: 0,
 };
 
 function scrollToTarget(selector = '') {
@@ -18,7 +20,7 @@ function registerObserver(selector) {
         let items = Array.from(document.querySelectorAll(`${selector}`));
         let currentItem;
         let pos, lastPos;
-        
+
         for (let e of items) {
             pos = e.getBoundingClientRect().top - window.innerHeight / 2;
             if (pos < 0) {
@@ -33,7 +35,7 @@ function registerObserver(selector) {
             }
         }
 
-        if(currentItem){
+        if (currentItem) {
             if (currentItem !== activeItem) {
                 document.querySelectorAll(`${selector}.active`).forEach(el => el.classList.remove('active'));
                 currentItem.classList.add('active');
@@ -45,13 +47,16 @@ function registerObserver(selector) {
         document.querySelectorAll(selector).forEach(ele => {
             ele.classList.add('obs-hover');
             ele.addEventListener('click', () => {
-                ele.scrollIntoView({behavior: "smooth", block: "center"});
+                ele.scrollIntoView({ behavior: "smooth", block: "center" });
             });
         });
     });
 }
 
 window.addEventListener('mousemove', e => {
+    aurigaConfigs.pointerX = e.pageX;
+    aurigaConfigs.pointerY = e.pageY;
+
     const elem = document.createElement('div');
     const spread = Math.round(Math.random() * 8) + 3;
     const blur = spread * (Math.round(Math.random() * 12) + 2);
@@ -65,6 +70,50 @@ window.addEventListener('mousemove', e => {
         elem.remove();
     }, 1000);
 });
+
+window.addEventListener('pointerdown', holdingEffects);
+window.addEventListener('pointerup', holdingEffects);
+
+var holdInt = '';
+function holdingEffects(e) {
+    if ('pointerdown' === e.type) {
+        aurigaConfigs.pointerX = e.pageX;
+        aurigaConfigs.pointerY = e.pageY;
+        holdInt = setInterval(() => {
+            const elem = document.createElement('div');
+            const spread = Math.round(Math.random() * 4) + 2;
+            const blur = spread * (Math.round(Math.random() * 6) + 3);
+            const hue = Math.random() * 180;
+            const brightness = Math.random() + 0.8;
+
+            const posX = aurigaConfigs.pointerX + Math.round(Math.random() * 50) -25;
+            const posY = aurigaConfigs.pointerY + Math.round(Math.random() * 60) -30;
+            elem.classList.add('absolute', 'bg-light-green', 'inline-block', 'rounded-full', 'play-dot-1');
+            elem.style = `top: ${posY}; left: ${posX}; box-shadow: 0 0 ${blur}px ${spread}px rgba(57, 240, 185, .9); filter: hue-rotate(${hue}deg) brightness(${brightness});`;
+
+            document.getElementById('bg-play').append(elem);
+            setTimeout(function () {
+                elem.remove();
+            }, 2000);
+        }, 100);
+    } else {
+        clearInterval(holdInt);
+    }
+}
+
+window.addEventListener('mousedown', () => {
+    if(is_rightclick()){
+        clearInterval(holdInt);
+    }
+});
+
+function is_rightclick() {
+    var rightclick;
+    var e = window.event;
+    if (e.which) rightclick = (e.which == 3);
+    else if (e.button) rightclick = (e.button == 2);
+    return rightclick;
+}
 
 // Set things up
 window.addEventListener("load", () => {
