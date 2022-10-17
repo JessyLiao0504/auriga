266bd904df99e404517a46851d729bae27d4d2a0
linsly-achang <76680239+linsly-achang@users.noreply.github.com>
2022-10-17 11:49:28 +0800

    dev

--- js/main.js
+++ js/main.js
@@ -159,13 +159,13 @@ document.addEventListener('DOMContentLoaded', () => {
     });
 });
 
-window.addEventListener('wheel', e => {
-    e.preventDefault();
-    window.scrollBy({
-        top: -e.wheelDelta*1.2,
-        behavior: 'smooth'
-    });
-}, { passive: false });
+// window.addEventListener('wheel', e => {
+//     e.preventDefault();
+//     window.scrollBy({
+//         top: -e.wheelDelta*1.2,
+//         behavior: 'smooth'
+//     });
+// }, { passive: false });
 
 // window.addEventListener('scroll', () => {
 //     document.body.style.setProperty('--scroll', window.scrollY / (document.body.offsetHeight - window.innerHeight));
